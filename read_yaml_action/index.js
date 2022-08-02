f08780322071f87dbd844df389255e3ac12ed4a7
Daniel J. Dean <dade@microsoft.com>
2022-08-02 13:49:47 -0400

    Update index.js

--- read_yaml_action/index.js
+++ read_yaml_action/index.js
@@ -10,9 +10,15 @@ try {
       return;
     }
     console.log(data);
-    const configYaml = yaml.load(data, 'utf8');
-    core.setOutput("resource_group", configYaml["variables"]["resource_group"]);
-    core.setOutput("aml_workspace", configYaml["variables"]["aml_workspace"]);
+    const SCHEMA = yaml.FAILSAFE_SCHEMA
+    const configYaml = yaml.load(data, { schema: SCHEMA }); 
+    const namespace = String(configYaml["variables"]["namespace"])
+    const postfix = String(configYaml["variables"]["postfix"])
+    const environment = String(configYaml["variables"]["environment"])
+    const resource_group = "rg-"+namespace+"-"+postfix+environment
+    const aml_workspace = "mlw-"+namespace+"-"+postfix+environment
+    core.setOutput("resource_group",resource_group);
+    core.setOutput("aml_workspace", aml_workspace);
   });
   
 } catch (error) {
