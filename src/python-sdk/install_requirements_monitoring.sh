2779b9defa43e05ee61d09dc5a40ff6e25af2e25
Nicole Serafino <74378487+nicoleserafino@users.noreply.github.com>
2022-09-01 14:27:18 -0500

    requirements

--- src/python-sdk/install_requirements_monitoring.sh
+++ src/python-sdk/install_requirements_monitoring.sh
@@ -2,10 +2,10 @@
 # Licensed under the MIT License.
 
 python --version
-pip install azureml-core==1.41.0
+pip install azureml-core==1.38.0
 pip install pandas==1.2.1
 pip install scikit-learn==0.24.1
-pip install azureml-defaults==1.41.0
+pip install azureml-defaults==1.38.0
 pip install azure-ai-ml==0.0.62653692 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 pip install --upgrade git+https://github.com/microsoft/AzureML-Observability#subdirectory=aml-obs-collector
 pip install --upgrade git+https://github.com/microsoft/AzureML-Observability#subdirectory=aml-obs-client
