ad3d365fbd4eb67870bff62d48dce69ca897f573
Nicole Serafino <74378487+nicoleserafino@users.noreply.github.com>
2022-09-01 11:46:22 -0500

    install requirements

--- src/python-sdk/install_requirements_monitoring.sh
+++ src/python-sdk/install_requirements_monitoring.sh
@@ -6,5 +6,6 @@ pip install azureml-sdk==1.41.0
 pip install azureml-dataset-runtime --upgrade
 pip install azure-ai-ml==0.0.62653692 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 pip install --upgrade git+https://github.com/microsoft/AzureML-Observability#subdirectory=aml-obs-collector
+pip install --upgrade git+https://github.com/microsoft/AzureML-Observability#subdirectory=aml-obs-client
 sudo apt install dotnet-runtime-deps-2.1
 sudo apt install dotnet-runtime-2.1
