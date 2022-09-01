59d8afac4e9bf35ec15af3ed482c6b2f2b8c6b63
Nicole Serafino <74378487+nicoleserafino@users.noreply.github.com>
2022-09-01 11:50:36 -0500

    add pandas to requirements

--- src/python-sdk/install_requirements_monitoring.sh
+++ src/python-sdk/install_requirements_monitoring.sh
@@ -4,6 +4,7 @@
 python --version
 pip install azureml-sdk==1.41.0
 pip install azureml-dataset-runtime --upgrade
+pip install pandas==1.2.1
 pip install azure-ai-ml==0.0.62653692 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 pip install --upgrade git+https://github.com/microsoft/AzureML-Observability#subdirectory=aml-obs-collector
 pip install --upgrade git+https://github.com/microsoft/AzureML-Observability#subdirectory=aml-obs-client
