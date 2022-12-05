1833086176891a9c77dc9695d6fa19c03ac254ab
msmarti <ms-andrem-hotmail.com>
2022-12-05 16:38:42 -0500

    gh

--- read_yaml_action/index.js
+++ read_yaml_action/index.js
@@ -18,15 +18,20 @@ try {
     const postfix = String(configYaml["variables"]["postfix"]);
     const environment = String(configYaml["variables"]["environment"]);
     var resource_group = String(configYaml["variables"]["resource_group"]);
+    var location = String(configYaml["variables"]["location"]);
     var aml_workspace = String(configYaml["variables"]["aml_workspace"]);
 
     var terraform_version = String(configYaml["variables"]["terraform_version"]);
     var terraform_workingdir = String(configYaml["variables"]["terraform_workingdir"]);
+    var terraform_st_location = String(configYaml["variables"]["terraform_st_location"]);
     var terraform_st_resource_group = String(configYaml["variables"]["terraform_st_resource_group"]);
     var terraform_st_storage_account = String(configYaml["variables"]["terraform_st_storage_account"]);
     var terraform_st_container_name = String(configYaml["variables"]["terraform_st_container_name"]);
     var terraform_st_key = String(configYaml["variables"]["terraform_st_key"]);
 
+    if(checkGenerateEntity(terraform_st_location)){
+      terraform_st_location = location;
+    }
     if(checkGenerateEntity(resource_group)){
         resource_group = "rg-"+namespace+"-"+postfix+environment;
     }
@@ -43,12 +48,14 @@ try {
 
     const batch_endpoint_name = "bep-"+namespace+"-"+postfix+environment;
     const online_endpoint_name = "oep-"+namespace+"-"+postfix+environment;
+    core.setOutput("location",location);
     core.setOutput("resource_group",resource_group);
     core.setOutput("aml_workspace", aml_workspace);
     core.setOutput("bep", batch_endpoint_name);
     core.setOutput("oep", online_endpoint_name);
     core.setOutput("terraform_version", terraform_version);
     core.setOutput("terraform_workingdir", terraform_workingdir);
+    core.setOutput("terraform_st_location", terraform_st_location);
     core.setOutput("terraform_st_resource_group", terraform_st_resource_group);
     core.setOutput("terraform_st_storage_account", terraform_st_storage_account);
     core.setOutput("terraform_st_container_name", terraform_st_container_name);
