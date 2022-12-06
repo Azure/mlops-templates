8776fec22d3d334c0ea52e1b204cb1c780114cd4
Maggie Mhanna <maggiemhanna@gmail.com>
2022-12-06 00:07:56 +0000

    sdkv2 templates create online endpoint

---
+++ src/python-sdk-v2/create_online_endpoint.py
@@ -0,0 +1,43 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import argparse
+
+from azure.ai.ml.entities import ManagedOnlineEndpoint
+
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml import MLClient
+
+import json
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Register dataset")
+    parser.add_argument("-n", type=str, help="Name of online endpoint")
+    parser.add_argument("-d", type=str, help="Description of the online endpoint")
+    parser.add_argument("-a", type=str, help="authentication mode", default="aml_token")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    print(args)
+    
+    credential = DefaultAzureCredential()
+    try:
+        ml_client = MLClient.from_config(credential, path='config.json')
+
+    except Exception as ex:
+        print("HERE IN THE EXCEPTION BLOCK")
+        print(ex)
+
+    # create an online endpoint
+    online_endpoint = ManagedOnlineEndpoint(
+        name=args.n, 
+        description=args.d,
+        auth_mode=args.a,
+    )
+    ml_client.online_endpoints.begin_create_or_update(
+        online_endpoint,   
+    )
+
+if __name__ == "__main__":
+    main()
