fe51de05e36dff5b2cb6e6753f103ebb2e19df16
Cindy Weng <8880364+cindyweng@users.noreply.github.com>
2022-12-08 10:03:15 +0000

    added conda/docker build type

--- src/python-sdk-v2/register_environment.py
+++ src/python-sdk-v2/register_environment.py
@@ -1,135 +1,55 @@
-"""MLOps v2 NLP Python SDK register environment script."""
-import os
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import argparse
-import traceback
 
-# Azure ML sdk v2 imports
-from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
-from azure.ai.ml import MLClient
 from azure.ai.ml.entities import Environment, BuildContext
-from azure.core.exceptions import ResourceExistsError
 
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml import MLClient
 
-def get_config_parger(parser: argparse.ArgumentParser = None):
-    """Builds the argument parser for the script."""
-    if parser is None:
-        parser = argparse.ArgumentParser(description=__doc__)
+import json
 
-    parser.add_argument(
-        "--subscription_id",
-        type=str,
-        required=False,
-        help="Subscription ID",
-    )
-    parser.add_argument(
-        "--build_type",
-        type=str,
-        required=False,
-        help="Resource group name",
-    )
-    parser.add_argument(
-        "--resource_group",
-        type=str,
-        required=False,
-        help="Resource group name",
-    )
-    parser.add_argument(
-        "--workspace_name",
-        type=str,
-        required=False,
-        help="Workspace name",
-    )
-    parser.add_argument(
-        "--exists_ok",
-        default=False,
-        action="store_true",
-        help="if True, will not fail if environment already exists",
-    )
+def parse_args():
+    parser = argparse.ArgumentParser(description="Register dataset")
+    parser.add_argument("-n", type=str, help="Name of the environment you want to register")
+    parser.add_argument("-d", type=str, help="Description of the environment")
+    parser.add_argument("-p", type=str, help="Local path of environment file(s)")
+    parser.add_argument("-b", type=str, help="Build type: either docker or conda")
+    return parser.parse_args()
 
-    parser.add_argument(
-        "--environment_name",
-        default="nlp_summarization_train",
-        type=str,
-    )
-    parser.add_argument(
-        "--environment_version",
-        default="mlopsv2-july2022",
-        type=str,
-    )
-    parser.add_argument(
-        "--environment_context_path",
-        default=os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "..",
-            "..",
-            "data-science",
-            "environments",
-            "training",
-        ),
-        type=str,
-    )
-    return parser
-
-
-def connect_to_aml(args):
-    """Connect to Azure ML workspace using provided cli arguments."""
+def main():
+    args = parse_args()
+    print(args)
+    
+    credential = DefaultAzureCredential()
     try:
-        credential = DefaultAzureCredential()
-        # Check if given credential can get token successfully.
-        credential.get_token("https://management.azure.com/.default")
-    except Exception as ex:
-        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
-        credential = InteractiveBrowserCredential()
-
-    # Get a handle to workspace
-    try:
-        # ml_client to connect using local config.json
         ml_client = MLClient.from_config(credential, path='config.json')
 
     except Exception as ex:
-        print(
-            "Could not find config.json, using config.yaml refs to Azure ML workspace instead."
-        )
+        print("Could not find config.json or config.json is not in the right format.")
+        print(ex)
 
-        # tries to connect using cli args if provided else using config.yaml
-        ml_client = MLClient(
-            subscription_id=args.subscription_id,
-            resource_group_name=args.resource_group,
-            workspace_name=args.workspace_name,
-            credential=credential,
-        )
-    return ml_client
-
-
-def main():
-    """Main entry point for the script."""
-    parser = get_config_parger()
-    args, _ = parser.parse_known_args()
-    ml_client = connect_to_aml(args)
-
-    
-    build_type = args.build_type
+    build_type = args.b
     if build_type == 'docker':
         print("Using docker build contect")
         custom_env = Environment(
-            name=args.environment_name,
-            build=BuildContext(path=args.environment_context_path),
-            tags={"project": "mlopsv2", "url": "https://github.com/Azure/mlops-v2"},
+            name=args.n,
+            build=BuildContext(path=args.p),
+            description=args.d
         )
+    elif build_type == 'conda':
+        my_environment = Environment(
+            image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04",
+            conda_file=args.p,
+            name=args.n,
+            description=args.d,
+        )
+    else: 
+        print("Expected 'docker' or 'conda' as build type.")
+        print(ex)
 
-        try:
-            custom_env_create_job = ml_client.environments.create_or_update(custom_env)
-            print(
-                f"Environment with name {custom_env_create_job.name} is registered to workspace, the environment version is {custom_env_create_job.version}"
-            )
-        except ResourceExistsError as ex:
-            print(f"Failed to create environment: {traceback.format_exc()}")
-            if not args.exists_ok:
-                raise
-    else:
-        print("Using conda definition")
-
+    ml_client.environments.create_or_update(my_environment)    
 
 if __name__ == "__main__":
     main()
