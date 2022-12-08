8dbe1cc9b18283f779965d02f51eeb76f0b86e64
Cindy Weng <8880364+cindyweng@users.noreply.github.com>
2022-12-08 14:44:20 +0000

    Update register_environment.py

--- src/python-sdk-v2/register_environment.py
+++ src/python-sdk-v2/register_environment.py
@@ -11,11 +11,12 @@ from azure.ai.ml import MLClient
 import json
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Register dataset")
-    parser.add_argument("-n", type=str, help="Name of the environment you want to register")
-    parser.add_argument("-d", type=str, help="Description of the environment")
-    parser.add_argument("-p", type=str, help="Local path of environment file(s)")
-    parser.add_argument("-b", type=str, help="Build type: either docker or conda")
+    parser = argparse.ArgumentParser(description="Register Environment")
+    parser.add_argument("--environment_name", type=str, help="Name of the environment you want to register")
+    parser.add_argument("--description", type=str, help="Description of the environment")
+    parser.add_argument("--env_path", type=str, help="Local path of environment file(s)")
+    parser.add_argument("--build_type", type=str, help="Build type: either docker or conda")
+    parser.add_argument("--base_image", type=str, help="base image path", default="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04")
     return parser.parse_args()
 
 def main():
@@ -30,26 +31,26 @@ def main():
         print("Could not find config.json or config.json is not in the right format.")
         print(ex)
 
-    build_type = args.b
+    build_type = args.build_type
     if build_type == 'docker':
         print("Using docker build contect")
-        my_environment = Environment(
-            name=args.n,
-            build=BuildContext(path=args.p),
-            description=args.d
+        environment = Environment(
+            name=args.environment_name,
+            build=BuildContext(path=args.env_path),
+            description=args.description
         )
     elif build_type == 'conda':
-        my_environment = Environment(
-            image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04",
-            conda_file=args.p,
-            name=args.n,
-            description=args.d,
+        environment = Environment(
+            image=args.base_image,
+            conda_file=args.env_path,
+            name=args.environment_name,
+            description=args.description
         )
     else: 
         print("Expected 'docker' or 'conda' as build type.")
         print(ex)
 
-    ml_client.environments.create_or_update(my_environment)    
+    ml_client.environments.create_or_update(environment)    
 
 if __name__ == "__main__":
     main()
